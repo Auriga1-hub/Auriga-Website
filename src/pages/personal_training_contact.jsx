5594e5a52f7ea4ef45d878bbb886cb21cdec61d0
Sathv <info@aurigafootballclub.com>
2026-03-26 17:51:35 +0530

    Fix missing CSS imports in trial pages and personal training contact page

--- src/pages/personal_training_contact.jsx
+++ src/pages/personal_training_contact.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import emailjs from "@emailjs/browser";
 import { EMAILJS_CONFIG } from "../utils/emailConfig";
+import "../css/contact.css";
 
 const TURNSTILE_SITE_KEY = "0x4AAAAAACuIL-SoeDNpEWX7";
 
